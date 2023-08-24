789c754c1bf9cb097e8afd0b4a0b3de7822b2f2f
StasX <s.mestechkin@gmail.com>
2023-08-25 00:44:52 +0300

    Fixed comment

--- 19a/app.py
+++ 19a/app.py
@@ -2,7 +2,7 @@
 user_input = input("Please enter positive number: ")
 number = int(user_input)
 
-# Print square of *
+# Print triangle of *
 for i in range(number, 0, -1):
     string = "* " * i
     print(string)
