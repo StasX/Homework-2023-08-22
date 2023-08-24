ab3858fdac9bd2d39da32d33a6290e2fe23a0cf1
StasX <s.mestechkin@gmail.com>
2023-08-24 22:33:20 +0300

    Added spaces around +

--- 11/app.py
+++ 11/app.py
@@ -3,5 +3,5 @@ user_input = input("Please a positive number: ")
 number = int(user_input)
 
 # Display numbers between 1 and number
-for i in range(1, number+1):
+for i in range(1, number + 1):
     print(i, end=" ")
