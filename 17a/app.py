e8c8324303e14812d2a36e90b1e5b5587d17fd7f
StasX <s.mestechkin@gmail.com>
2023-08-24 23:38:03 +0300

    Add solution of exercise #17a

---
+++ 17a/app.py
@@ -0,0 +1,8 @@
+# Get number from user
+user_input = input("Please enter positive number: ")
+number = int(user_input)
+
+# Print square of *
+for i in range(number):
+    string = "* " * number
+    print(string)
