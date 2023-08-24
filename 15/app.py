cf1a56694c9ecee1675e48d123017be8e29bb59c
StasX <s.mestechkin@gmail.com>
2023-08-24 23:14:18 +0300

    Add solution of exercise #15

---
+++ 15/app.py
@@ -0,0 +1,20 @@
+# Print message to user
+print("Enter two numbers (First should be smaller).")
+
+# Get first number from user
+first_input = input("Please enter first number: ")
+first = int(first_input)
+
+# Get second number from user
+second_input = input("Please enter second number: ")
+second = int(second_input)
+
+# If first number greater than second then swap
+if first > second:
+    temp = first
+    first = second
+    second = temp
+
+# Print numbers from first number to second number
+for i in range(first, second+1):
+    print(i, end=" ")
